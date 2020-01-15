5fe03bd30fea3ae99b7aaef4a1ae4902f4b53bb4
DLillard10UTM <danllill@ut.utm.edu>
2020-01-15 16:33:24 -0600

    My file was in the wrong space, because my Program.cs file was not included with the NewRepo, on my "Hello World" update only contained the .gitignore and .getattributes, thus I am adding this Hello World code second.

--- Program.cs
+++ Program.cs
@@ -10,28 +10,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            ///word
-            int numTries = 0;
-            Random numGen = new Random();
-            int num = numGen.Next(1, 101);
-            Console.WriteLine("Hello, I am thinking of a number, from 1 to 100, can you guess it?");
-            Console.WriteLine("Enter Guess: ");
-
-            string userInput = Console.ReadLine();
-            numTries++;
-            while(Convert.ToInt32(userInput) != num) {
-                if (Convert.ToInt32(userInput) < num)
-                    Console.WriteLine("Too Low!");
-
-                if (Convert.ToInt32(userInput) > num)
-                    Console.WriteLine("Too High!");
-
-                Console.WriteLine("Enter Guess: ");
-                userInput = Console.ReadLine();
-                numTries++;
-            }
-            Console.WriteLine("You've Won! It took you: " + numTries + " tries to correctly guess the number! (Press any button to end)");
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
